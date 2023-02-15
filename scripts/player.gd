eedcf6a490c1c70585b18d8d2eb9642848c15c29
LastGenius-edu <as.andreysultanov@gmail.com>
2023-02-15 16:15:00 +0000

    Add smooth player movement

---
+++ scripts/player.gd
@@ -0,0 +1,61 @@
+extends KinematicBody
+
+export var interpolation_rate = 8
+export var rotation_rate = 5
+
+export var shift_speed = 14
+export var speed = 9
+
+export var req_rotation = 35
+
+var velocity = Vector3.ZERO
+var camera: Camera
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	camera = get_node("/root/Main/Camera Pivot/Camera")
+
+
+static func _short_angle_dist(from, to):
+	var max_angle = PI * 2
+	var difference = fmod(to - from, max_angle)
+	return fmod(2 * difference, max_angle) - difference
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	var cam_basis = camera.global_transform.basis
+	var a: Quat
+	var b: Quat
+	
+	var input_vector = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	direction += input_vector.x * cam_basis.x
+	direction += input_vector.y * cam_basis.z
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		
+		# Determine new target position
+		var new_position = global_translation + Vector3(direction.x, 0.0, direction.z)
+		
+		# Interpolate to the new rotation
+		a = Quat(global_transform.basis)
+		b = Quat(global_transform.looking_at(new_position, Vector3.UP).basis)
+		global_transform.basis = Basis(a.slerp(b, rotation_rate * delta))
+		
+		# Increase movement intensity as we get closer to the right rotation
+		var movement_intensity: float = pow(abs((clamp(rad2deg(a.angle_to(b)), 0, req_rotation) / req_rotation) - 1), 2)
+		
+		# Only move if we have < req_rotation to go
+		if movement_intensity > 0.01:
+			direction *= movement_intensity
+			print(rad2deg(a.angle_to(b)), " | ", movement_intensity)
+			if Input.is_action_pressed("sprint"):
+				direction *= shift_speed
+			else:
+				direction *= speed
+				
+			var interpolated = velocity.linear_interpolate(direction, interpolation_rate * delta)
+			velocity.x = interpolated.x
+			velocity.z = interpolated.z
+			
+			velocity = move_and_slide(velocity, Vector3.UP)
